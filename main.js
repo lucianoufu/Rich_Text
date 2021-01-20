69fb543343c80019d2fcd1771fe14c6d2b6ccfd6
Luciano <soaresluciano85@gmail.com>
2021-01-19 22:40:53 -0300

    Add function to get and change selected text

--- main.js
+++ main.js
@@ -1,4 +1,16 @@
 function mudaTexto(){
     var textoId = document.getElementById("TextAreaId").value;
     console.log(textoId);
+    var paragraph = document.getElementById("Text");
+    paragraph.innerText = textoId;
+
+}
+
+function mudaEstiloTexto(){
+    /* var teste = window.document.getSelection(); */
+    var teste = window.document.getSelection();
+    documento = document.getElementById("Text");
+    var novavar = documento.innerText.replace(teste, "Teste 123");
+    documento.innerText = novavar;
+    
 }
