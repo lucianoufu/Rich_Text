a1d20cf746dedefac1390d3314bb94f63dbed327
Luciano <soaresluciano85@gmail.com>
2021-01-21 19:54:56 -0300

    Setting up the color setings

--- main.js
+++ main.js
@@ -14,27 +14,7 @@ function mudaEstiloTexto(){
     documento.innerText = novavar;
 }
 
-/* function fn_muda_estilo_texto(formatacao){
-    var textoSelecionado = document.getSelection();
-    var textoHTML = document.getElementById("Text");
-    if (formatacao == 'a'){
-        url = window.prompt('Digite a sua url: ');
-        textoFormatado = textoHTML.innerText.replace(textoSelecionado, `<${formatacao} href = "${url}"> ${textoSelecionado} </${formatacao}>`);
-    }
-    else{
-        textoFormatado = textoHTML.innerText.replace(textoSelecionado, `<${formatacao}> ${textoSelecionado} </${formatacao}>`);
-    }
-
-    textoFormatado = `${textoFormatado}`
-    textoFormatado.replace('<p id = "Text">', '');
-    textoFormatado.replace('</p>', '');
-    console.log(`Texto selecionado: ${textoSelecionado}`);
-    console.log(`Texto HTML: ${textoHTML}`);
-    console.log(`Texto Formatado: ${textoFormatado}`);
-    textoHTML.innerHTML += ' ' + textoFormatado;
-} */
-
-function fn_muda_estilo_texto(formatacao){
+function fn_muda_estilo_texto(formatacao, color){
     var texto_selecionado = document.getSelection();
     var textohtml = document.getElementById("Text");
     if (formatacao == 't'){
@@ -48,9 +28,30 @@ function fn_muda_estilo_texto(formatacao){
         textohtml.innerHTML = textohtml.innerHTML.replace(texto_selecionado, `<div class = 'specialArea'> <p>${texto_selecionado}</p></div>`);
     }
     else if (formatacao == 'c'){
-        textohtml.innerHTML = textohtml.innerHTML.replace(texto_selecionado, `<span style = "color: #2a6099">${texto_selecionado}</span>`);
+        if(color == 'yellow'){
+            textohtml.innerHTML = textohtml.innerHTML.replace(texto_selecionado, `<span style = "color: #e0e012">${texto_selecionado}</span>`);
+        }
+
+        else if (color == 'blue'){
+            textohtml.innerHTML = textohtml.innerHTML.replace(texto_selecionado, `<span style = "color: #2a6099">${texto_selecionado}</span>`);
+        }
+        else if (color == 'red'){
+            textohtml.innerHTML = textohtml.innerHTML.replace(texto_selecionado, `<span style = "color: #FF0000">${texto_selecionado}</span>`);
+        }
+        else if (color == 'green'){
+            textohtml.innerHTML = textohtml.innerHTML.replace(texto_selecionado, `<span style = "color: #08b308">${texto_selecionado}</span>`);
+        }
+        else if (color == 'pink'){
+            textohtml.innerHTML = textohtml.innerHTML.replace(texto_selecionado, `<span style = "color: #FF00FF">${texto_selecionado}</span>`);
+        }
     }
     else{
         textohtml.innerHTML = textohtml.innerHTML.replace(texto_selecionado, `<${formatacao}>${texto_selecionado}</${formatacao}>`);
     }   
+}
+
+function color_selected(){
+    var color = document.getElementById("mySelect").value;
+    console.log(color);
+    fn_muda_estilo_texto('c', color);
 }
