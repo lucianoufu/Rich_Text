4f019b82c9edf0362a45fb46f221bf3696f99a27
Luciano <soaresluciano85@gmail.com>
2021-01-21 21:32:36 -0300

    Change variables name to English

--- main.js
+++ main.js
@@ -1,57 +1,49 @@
-function mudaTexto(){
-    var textoId = document.getElementById("TextAreaId").value;
-    console.log(textoId);
+function change_text(){
+    var textId = document.getElementById("TextAreaId").value;
+    console.log(textId);
     var paragraph = document.getElementById("Text");
-    paragraph.innerText = textoId;
+    paragraph.innerText = textId;
 
 }
 
-function mudaEstiloTexto(){
-    /* var teste = window.document.getSelection(); */
-    var teste = window.document.getSelection();
-    documento = document.getElementById("Text");
-    var novavar = documento.innerText.replace(teste, "Teste 123");
-    documento.innerText = novavar;
-}
-
-function fn_muda_estilo_texto(formatacao, color){
-    var texto_selecionado = document.getSelection();
-    var textohtml = document.getElementById("Text");
-    if (formatacao == 't'){
-        textohtml.innerHTML = textohtml.innerHTML.replace(texto_selecionado, `<h2>${texto_selecionado}</h2>`);
+function fn_change_text_style(format, color){
+    var selected_text = document.getSelection();
+    var textHTMLY = document.getElementById("Text");
+    if (format == 't'){
+        textHTMLY.innerHTML = textHTMLY.innerHTML.replace(selected_text, `<h2>${selected_text}</h2>`);
     }
-    else if (formatacao == 'a'){
+    else if (format == 'a'){
         url = window.prompt('Digite a url.');
-        textohtml.innerHTML = textohtml.innerHTML.replace(texto_selecionado, `<${formatacao} href = "${url}">${texto_selecionado}</${formatacao}>`);
+        textHTMLY.innerHTML = textHTMLY.innerHTML.replace(selected_text, `<${format} href = "${url}">${selected_text}</${format}>`);
     }
-    else if (formatacao == 's'){
-        textohtml.innerHTML = textohtml.innerHTML.replace(texto_selecionado, `<div class = 'specialArea'> <p>${texto_selecionado}</p></div>`);
+    else if (format == 's'){
+        textHTMLY.innerHTML = textHTMLY.innerHTML.replace(selected_text, `<div class = 'specialArea'> <p>${selected_text}</p></div>`);
     }
-    else if (formatacao == 'c'){
-        if(color == 'yellow'){
-            textohtml.innerHTML = textohtml.innerHTML.replace(texto_selecionado, `<span style = "color: #e0e012">${texto_selecionado}</span>`);
+    else if (format == 'c'){
+        if(color == 'orange'){
+            textHTMLY.innerHTML = textHTMLY.innerHTML.replace(selected_text, `<span style = "color: #ff8800">${selected_text}</span>`);
         }
 
         else if (color == 'blue'){
-            textohtml.innerHTML = textohtml.innerHTML.replace(texto_selecionado, `<span style = "color: #2a6099">${texto_selecionado}</span>`);
+            textHTMLY.innerHTML = textHTMLY.innerHTML.replace(selected_text, `<span style = "color: #2a6099">${selected_text}</span>`);
         }
         else if (color == 'red'){
-            textohtml.innerHTML = textohtml.innerHTML.replace(texto_selecionado, `<span style = "color: #FF0000">${texto_selecionado}</span>`);
+            textHTMLY.innerHTML = textHTMLY.innerHTML.replace(selected_text, `<span style = "color: #FF0000">${selected_text}</span>`);
         }
         else if (color == 'green'){
-            textohtml.innerHTML = textohtml.innerHTML.replace(texto_selecionado, `<span style = "color: #08b308">${texto_selecionado}</span>`);
+            textHTMLY.innerHTML = textHTMLY.innerHTML.replace(selected_text, `<span style = "color: #08b308">${selected_text}</span>`);
         }
         else if (color == 'pink'){
-            textohtml.innerHTML = textohtml.innerHTML.replace(texto_selecionado, `<span style = "color: #FF00FF">${texto_selecionado}</span>`);
+            textHTMLY.innerHTML = textHTMLY.innerHTML.replace(selected_text, `<span style = "color: #FF00FF">${selected_text}</span>`);
         }
     }
     else{
-        textohtml.innerHTML = textohtml.innerHTML.replace(texto_selecionado, `<${formatacao}>${texto_selecionado}</${formatacao}>`);
+        textHTMLY.innerHTML = textHTMLY.innerHTML.replace(selected_text, `<${format}>${selected_text}</${format}>`);
     }   
 }
 
 function color_selected(){
     var color = document.getElementById("mySelect").value;
     console.log(color);
-    fn_muda_estilo_texto('c', color);
+    fn_change_text_style('c', color);
 }
